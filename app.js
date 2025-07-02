f2513d8d479d73b49ebe4ecf10abef0675dcda65
Prince-Makvana <princepatel2274@gmail.com>
2025-07-02 09:41:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
 const express = require("express");
 const connectDB = require('./db');
 const app = express();
